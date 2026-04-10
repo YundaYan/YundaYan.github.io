20b7f688a4d24be64a8c411c00573462c58bb224
YundaYan <145666572+YundaYan@users.noreply.github.com>
2026-04-10 10:09:45 +0000

    Deploying to gh-pages from @ YundaYan/YundaYan.github.io@c9d9166832fa062b03b60bcfad3a21361323a144 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,16 +92,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-joined-university-college-london-ucl-as-a-lecturer-in-robotics-and-ai",
+          title: 'I joined University College London (UCL) as a Lecturer in Robotics and AI....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
