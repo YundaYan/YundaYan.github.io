547b97afa7e93001f288a14c35ec4e3465336b52
YundaYan <145666572+YundaYan@users.noreply.github.com>
2026-04-10 10:26:19 +0000

    Deploying to gh-pages from @ YundaYan/YundaYan.github.io@cdc40ab7d0cc2104ad6521e81c8fec168b1cb7b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,10 +92,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-joined-university-college-london-ucl-as-a-lecturer-in-robotics-and-ai",
+            },},{id: "news-i-joined-university-college-london-ucl-as-a-lecturer-in-robotics-and-ai",
           title: 'I joined University College London (UCL) as a Lecturer in Robotics and AI....',
           description: "",
           section: "News",},{id: "projects-project-1",
