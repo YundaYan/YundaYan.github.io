26b253bfd0733f4a04e4b5700638dd55549c7615
YundaYan <145666572+YundaYan@users.noreply.github.com>
2026-04-13 08:29:50 +0000

    Deploying to gh-pages from @ YundaYan/YundaYan.github.io@fc022e58278daf25244d3d52d3ac34759bfff12f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "For a complete list of publications, please refer to my Google Scholar profile or the UCL publications page.",
+          description: "For a complete list of publications, please refer to my  Google Scholar profile  or the  UCL publications page.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
