3300e9f5e85cac64a3c146b69a3ae6dc9c891650
YundaYan <145666572+YundaYan@users.noreply.github.com>
2026-04-14 10:36:24 +0000

    Deploying to gh-pages from @ YundaYan/YundaYan.github.io@9165e94e4f290584ef1a2c32ba0e6cd7c8fa79ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
@@ -36,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
